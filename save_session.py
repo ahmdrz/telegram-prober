ec822830d72e333e8d830c2c38a82b403c69dc15
Ahmadreza Zibaei <ahmadrezazibaei@hotmail.com>
2021-07-27 13:46:52 +0430

    init helm-chart branch

--- save_session.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from telegram.client import get_client, start_client
-
-client = get_client()
-client.loop.run_until_complete(start_client(client, phone=sys.argv[1]))
-session = client.session.save()
-print(session)
