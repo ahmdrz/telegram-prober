ae8813ace7c7731d97b456be0d59cb523206ac51
Ahmadreza Zibaei <zibaeiahmadreza@gmail.com>
2021-09-22 14:53:32 +0330

    new version release

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+set -e
+
 git clone https://github.com/ahmdrz/telegram-prober temp
 mv temp/helm-chart telegram-prober
 rm -rf temp
-helm package telegram-prober
+helm package telegram-prober --version $HELM_VERSION
 rm -rf telegram-prober
 helm repo index . --url https://ahmdrz.github.io/telegram-prober
